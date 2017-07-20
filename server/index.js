5250026a5ba83e3d83c36cc826eb7232a463b5ac
Noah Katz <noah.katz@ibm.com>
2017-07-20 19:39:17 -0400

    new requests

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var request=require('request-promise');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+var baseBlockURL="http://testhyp.localtunnel.me/api/";
 app.listen(8080, function () {
   console.log('listening on port 8080');
 })
@@ -56,7 +57,7 @@ app.post('/login', function (req, res) {
         }
         userData = JSON.parse(data);
         var userList = userData.users;
-
+    
         var validEntry = false;
         for (i in userList){
             var user = userList[i];
@@ -83,3 +84,93 @@ app.post('/login', function (req, res) {
         res.send(JSON.stringify(response));
     });
 })
+
+app.post('/addFunds', function (req, res) {    
+    var fundData={
+        "$class": "org.acme.mynetwork.AddFunds",
+        "amount": 100,
+        "roomate": "Alice",
+    }
+    var fundReq ={
+        method: "POST",
+        uri: baseBlockURL+"AddFunds",
+        headers: {
+                    'content-type': 'application/json' 
+            },
+        body: JSON.stringify(fundData)
+    }
+    request(fundReq).then(function(body){
+      //res
+    }).catch(function(err){
+      //err
+    });
+
+})
+
+
+app.post('/createItem', function (req, res) {
+    var itemURL=baseBlockURL+"Item"
+    var itemName="jafjkl";
+    var price=200;
+    var userId="Alice";
+    console.log(itemURL);
+    var checkExists={
+        method: 'GET',
+        uri: itemURL+"/"+itemName
+    }
+    request(checkExists).then(function(body){
+        var itemData={
+          "$class": "org.acme.mynetwork.Item",
+          "purchaseName": itemName,
+          "Price":price,
+          "owner": "Alice"
+        }
+        
+        var postItem={
+            method: 'PUT',
+            uri: itemURL+"/"+itemName,
+            headers: {
+                    'content-type': 'application/json' 
+            },
+            body: JSON.stringify(itemData)
+        }
+        request(postItem).then(function(body){
+            //res
+        }).catch(function(err){
+            console.log(err);
+        })
+
+
+
+
+
+
+    }).catch(function(err){
+        var itemData={
+          "$class": "org.acme.mynetwork.Item",
+          "purchaseName": itemName,
+          "Price":price,
+          "owner": "Alice"
+        }
+        
+        var postItem={
+            method: 'POST',
+            uri: itemURL,
+            headers: {
+                    'content-type': 'application/json' 
+            },
+            body: JSON.stringify(itemData)
+        }
+        request(postItem).then(function(body){
+            //res
+        }).catch(function(err){
+            console.log(err);
+        })
+
+    });
+})
+
+
+
+
+
